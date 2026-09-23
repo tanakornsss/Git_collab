a18f1160a2ff6a249de8808bab6bb8462a44e123
nattaken <nattapatboonpirom@gmail.com>
2026-09-23 17:05:49 +0700

    Dev B first commit

--- Hello.c
+++ Hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void) {
-    greeting(“Developer A,B,C !!”);
+    greeting("Developer A,B,C !!");
+    farewell("Developer A,B,C");
     return 0;
 }
 
@@ -8,5 +9,11 @@ int main(void) {
 int greeting(const char *name)
 {
     printf("Hello, %s!\n", name);
+    return 0;
+}
+
+int farewell(const char *name)
+{
+    printf("Goodbye, %s!\n", name);
     return 0;
 }
