d0611163f60e781487e20f5e6825eebdb1b82720
tanakornsss <omtanakorn2550@gmail.com>
2026-09-23 17:15:15 +0700

    Add checkin

--- Hello.c
+++ Hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main(void) {
+    checkin("Developer A,B,C !!");
     greeting("Developer A,B,C !!");
     farewell("Developer A,B,C");
     return 0;
@@ -16,5 +17,10 @@ int greeting(const char *name)
 int farewell(const char *name)
 {
     printf("Goodbye, %s!\n", name);
+    return 0;
+}
+
+int checkin(const char *name) {
+    printf("%s are checked in\n", name);
     return 0;
 }
