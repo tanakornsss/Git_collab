04de73c60a615bc591ee79e7785c043ab8d5ff0b
Korlols <korlol.urn@gmail.com>
2026-09-23 16:57:00 +0700

    Commit Dev A 1

--- Hello.c
+++ Hello.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(void) {
-    printf("Hello, Git Collaboration!\n");
+    greeting(“Developer A,B,C !!”);
+    return 0;
+}
+
+int greeting(const char *name)
+{
+    printf("Hello, %s!\n", name);
     return 0;
 }
